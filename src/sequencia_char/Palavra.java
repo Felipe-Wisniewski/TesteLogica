42611be0820d446a57b5dfaf3eb8554c5b672e74
Felipe Wisniewski <felipewisniewski@gmail.com>
2018-07-19 09:56:47 -0300

    teste sequência v1

---
+++ src/sequencia_char/Palavra.java
@@ -0,0 +1,31 @@
+package sequencia_char;
+/** 
+ * @author Felipe Wisniewski
+ */
+public class Palavra {
+
+	private String palavra;
+
+	public Palavra(String palavra) {
+		this.palavra = palavra;
+	}
+
+	public String getPalavra() { return palavra; }
+
+	public void setPalavra(String palavra) { this.palavra = palavra; }
+	
+	public int tamanhoPalavra() { return this.palavra.length();	}
+	
+	public char letraPalavra(int i) {
+		/**Método que retorna o carácter que se encontra no índice desejado.
+		 * @param i - inteiro referente ao index.
+		 * @return c - char da posição requerida.  
+		 */
+		if(i == tamanhoPalavra()) {
+			return '*';
+		}
+		char c = this.palavra.charAt(i);
+		return c;
+	}
+	
+}
--- src/sequencia_char/TesteSequencia.java
+++ src/sequencia_char/TesteSequencia.java
@@ -1,10 +1,55 @@
 package sequencia_char;
 
+import java.util.Scanner;
+
+/**Teste de lógica: Dada uma palavra, escreva um algoritmo que retorne a sequência de caracteres que mais 
+ * aparecem em sequência. Ex.: "Pressaaao", deve retornar "aaa". 
+ * 
+ * @author Felipe Wisniewski
+ * @version 1.0
+ */
+
 public class TesteSequencia {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Scanner scanner;
 
+	public static void main(String[] args) {
+		String palavra;
+		
+		System.out.println("Entre com a palavra: ");
+		scanner = new Scanner(System.in);
+		palavra = scanner.nextLine(); 
+		
+		Palavra p = new Palavra(palavra);
+		
+		System.out.println(sequenciaCaracteres(p));		
+
+	}
+	
+	public static String sequenciaCaracteres(Palavra pa) {
+	/**Método que retorna a sequência de caracteres que mais aparecem.
+	 * @author Felipe Wisniewski
+	 * @param pa - Objeto do tipo Palavra
+	 * @return sequencia - String de caracteres. 
+	 */
+		
+		String aux, sequencia = "";
+		
+		for(int i = 0; i < pa.tamanhoPalavra(); i++) {
+			
+			aux = Character.toString(pa.letraPalavra(i));
+			
+			while(pa.letraPalavra(i) == pa.letraPalavra(i+1)) {				
+				aux += pa.letraPalavra(i+1);
+				i++;				
+			}
+			
+			if(aux.length() > sequencia.length()) {
+				sequencia = aux;
+			}			
+		}		
+		
+		return sequencia;
 	}
 
 }
