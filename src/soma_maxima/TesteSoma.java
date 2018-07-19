3ad33568c4e9501a43871b0f7361e5a8af92d889
Felipe Wisniewski <felipewisniewski@gmail.com>
2018-07-19 11:31:36 -0300

    teste soma v2

--- src/soma_maxima/TesteSoma.java
+++ src/soma_maxima/TesteSoma.java
@@ -30,42 +30,32 @@ public class TesteSoma {
 	private static ArrayList<String> subconjuntoSomaMaxima(ArrayList<Integer> s) {
 		
 		ArrayList<String> sub = new ArrayList<>();
-		int maxTotal, maxAtual, n;
+		int maxTotal, maxAtual, idx, idxI, idxF;
 		maxAtual = 0;
 		maxTotal = -1;
-		n=0;
+		idx = 0;
+		idxI = 0;
+		idxF = 0;	
 		
 		for(int i = 0; i < s.size(); i++) {
 			maxAtual = maxAtual + s.get(i);
 			
-			//Se a soma for negativa, zera o valor
-			if(maxAtual < 0) {
-				maxAtual = 0;
-				sub.clear();
-				n=0;
+			if(s.get(i) > maxAtual) {
+				maxAtual = s.get(i);
+				idx = i;			
 			}				
-			
-			
+						
 			if(maxAtual > maxTotal) {
 				maxTotal = maxAtual;
-				
-				if(n == 0) {
-					sub.add("**" + s.get(i).toString());
-					n++;
-				}else if(n == 1) {
-					sub.add(s.get(i).toString() + "**");
-					n++;
-				}else {
-					sub.set(sub.size() - 1, s.get(i - 1).toString());
-					sub.add(s.get(i).toString() + "**");
-				}
-				
-			}else {
-				sub.add(s.get(i).toString());
+				idxI = idx;
+				idxF = i;
 			}
-			
+			sub.add(s.get(i).toString());
 		}		
-			
+		
+		sub.set(idxI, "**" + s.get(idxI).toString());
+		sub.set(idxF, s.get(idxF).toString() + "**");
+		
 		return sub;
 	}
 
