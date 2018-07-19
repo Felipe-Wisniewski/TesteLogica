e91cbd948391a48fc059abd4ba4e90b3d92f8667
Felipe Wisniewski <felipewisniewski@gmail.com>
2018-07-19 12:12:34 -0300

    teste soma v3

--- src/soma_maxima/TesteSoma.java
+++ src/soma_maxima/TesteSoma.java
@@ -1,7 +1,17 @@
 package soma_maxima;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+/**Teste de lógica: Dado um conjunto de números, descobrir o subconjunto em que a soma dos elementos são de 
+ * máxima soma.
+ * Exemplo: dado o conjunto de elementos [2, -4, 6, 8, -10, 100, -6, 5]
+ * O subconjunto de soma máxima é: [2, -4, **6, 8, -10, 100**, -6, 5]
+ * 
+ * @author Felipe Wisniewki
+ * @version 1.1
+ */
 
 public class TesteSoma {
 
@@ -9,25 +19,43 @@ public class TesteSoma {
 	
 	public static void main(String[] args) {
 		
-		Integer n;
+		Integer n = 0;
 		ArrayList<Integer> sequencia = new ArrayList<>();
 		
-		System.out.println("Digite um número inteiro e aperte enter ou 00 para sair: ");
+		System.out.println("Digite um número inteiro e aperte enter ou '9999' para parar: ");
 		
+		//Preenche o conjunto de inteiros
 		do{
 			scanner = new Scanner(System.in);
-			n = scanner.nextInt();
 			
-			if(n != 00)
-				sequencia.add(n);
+			try {
+				n = scanner.nextInt();
+				
+				if(n != 9999)
+					sequencia.add(n);
+	
+			} catch (InputMismatchException e) {
+				scanner.nextLine();
+				System.out.println("Digite apenas números Inteiros!!");
+			}			
 			
-		}while(n != 00);
-		
+			if(n == 9999  && sequencia.isEmpty()) {
+				System.out.println("Não pode sair com o conjunto vazio!!");
+				n = 0;
+			}
+			
+		}while(n != 9999);
+				
 		System.out.println(subconjuntoSomaMaxima(sequencia));
 
 	}
 
 	private static ArrayList<String> subconjuntoSomaMaxima(ArrayList<Integer> s) {
+	/**Método que recebe o conjunto de números e retorna o subconjunto identificado por '**'
+	 * @author Felipe Wisniewski
+	 * @param s - ArrayList<Integer> (Conjunto de inteiros)
+	 * @return sub - ArrayList<String> (Resultado com a identificação do subconjunto)
+	 */
 		
 		ArrayList<String> sub = new ArrayList<>();
 		int maxTotal, maxAtual, idx, idxI, idxF;
