62b1f73b6aeed25dd7c2fc2098a03d27c9f3231d
Felipe Wisniewski <felipewisniewski@gmail.com>
2018-07-19 09:34:48 -0300

    Teste de logica

---
+++ src/sequencia_char/TesteSequencia.java
@@ -0,0 +1,10 @@
+package sequencia_char;
+
+public class TesteSequencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/soma_maxima/TesteSoma.java
@@ -0,0 +1,10 @@
+package soma_maxima;
+
+public class TesteSoma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
